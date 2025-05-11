81cdefa298113d8791b571f9a4bbb4a64340e3eb
Vasiliy Olchovik <olchovik2505@gmail.com>
2025-05-11 18:14:20 +0700

    Изменена на обычное деление

--- Vasiliy.py
+++ Vasiliy.py
@@ -3,7 +3,7 @@ def calculate_sum_and_average(numbers):
     for num in numbers:
         total_sum += num
 
-    average = total_sum // len(numbers)
+    average = total_sum / len(numbers)
     return total_sum, average
 
 numbers = [10, 20, 30, 40, 50]
@@ -14,4 +14,4 @@ print(f'Среднее значение: {avg}')
 numbers = [11, 22, 33, 42, 51]
 total, avg = calculate_sum_and_average(numbers)
 print(f'Сумма чисел: {total}')
-print(f'Среднее значение: {avg}') 
+print(f'Среднее значение: {avg}')
